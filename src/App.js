3c575e4125865d36f3f77d2b7b24858deff66ccd
Asaad Budayr <asad_289@hotmail.com>
2018-11-11 03:11:39 -0800

    Feat: get all books from the book API
    get the books and then pass them to the main and search page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import * as BooksAPI from './BooksAPI'
-import { Route } from 'react-router-dom';
-import './App.css';
-import Main from './components/pages/Main'
-import Search from './components/pages/Search'
+import { Route } from "react-router-dom";
+import "./App.css";
+import * as BooksAPI from "./BooksAPI";
+import Main from "./components/pages/Main";
+import Search from "./components/pages/Search";
 
 class App extends Component {
   state = {
     books: []
+  };
+  //get all the books from the books API
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      //console.log(books);
+      this.setState({ books });
+    });
   }
   render() {
+    console.log(this.state);
     return (
       <div className="App">
-        <Route exact path="/" component={Main}/>
-        <Route exact path="/search" component={Search}/>
+        <Route
+          exact
+          path="/"
+          render={() => <Main books={this.state.books} />}
+        />
+        <Route
+          exact
+          path="/search"
+          render={() => <Search books={this.state.books} />}
+        />
       </div>
     );
   }
