0d2ac98582ea4bdceedba82c271a18fbdbfc4d71
Asaad Budayr <asad_289@hotmail.com>
2018-11-12 02:04:15 -0800

    Feat: add an add book link

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,20 +1,23 @@
-import React, { Component } from 'react';
-// import * as BooksAPI from './BooksAPI'
-import Shelf from '../Shelf'
+import React, { Component } from "react";
+import Shelf from "../Shelf";
 
+import { Link } from "react-router-dom";
 //this component displays the main page with all the shelfs
 class Main extends Component {
   render() {
-    const {books} = this.props;
+    const { books } = this.props;
     //console.log(this.props);
-    return(
+    return (
       <div>
-          <div className="list-books-title">
-            <h1>MyReads</h1>
-          </div>
-          <Shelf books={books}/>
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <Shelf books={books} />
+        <div className="open-search">
+          <Link to="/search">Add a book</Link>
+        </div>
       </div>
-    )
+    );
   }
 }
-export default Main
+export default Main;
